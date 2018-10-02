0ef2976b983e02474942b7d0563eb444f54406ef
Ibrahim Hamed <39784258+ibrahimhamed20@users.noreply.github.com>
2018-10-02 12:11:26 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,8 +5,7 @@ app.config(function ($routeProvider) {
         .when("/", {templateUrl : "pages/main.htm", controller  : "mainCtrl"})
         .when("/users", {templateUrl : "pages/users.htm", controller  : "userCtrl"})
         .when("/orders", {templateUrl : "pages/orders.htm", controller  : "orderCtrl"})
-        .when("/mails", {templateUrl : "pages/products.htm", controller  : "productCtrl"})
-        .otherwise("#", {template : "<h1 class='text-center'>None</h1><p class='text-center'>Nothing has been selected</p>"});
+        .when("/mails", {templateUrl : "pages/products.htm", controller  : "productCtrl"});
 });
 
 app.controller("mainCtrl", function ($scope) {
